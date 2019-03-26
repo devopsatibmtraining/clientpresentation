9f4bb15d9af557faccd8a4b8eb4cec257c3b6032
RONALD ZAIDE <zaider@ph.ibm.com>
2019-03-27 00:50:45 +0800

    update 5

--- src/test/java/Test_Login.java
+++ src/test/java/Test_Login.java
@@ -12,11 +12,6 @@ public class Test_Login {
     public void test1(){
         guru_login check = new guru_login();
         assertEquals("Login is valid", true, check.isUserValid("medyo","sprint"));
-    }
-
-    @Test
-    public void test2(){
-        guru_login check = new guru_login();
         assertEquals("Login is valid", true, check.isUserValid("wrong","mistake"));
     }
 }
