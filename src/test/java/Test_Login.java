b7ce6697d6b519808c7aff0c934c1cc510f49ef4
RONALD ZAIDE <zaider@ph.ibm.com>
2019-03-27 00:49:09 +0800

    update 5

--- src/test/java/Test_Login.java
+++ src/test/java/Test_Login.java
@@ -1,6 +1,7 @@
 package test.java;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
 import main.java.*;
@@ -11,5 +12,11 @@ public class Test_Login {
     public void test1(){
         guru_login check = new guru_login();
         assertEquals("Login is valid", true, check.isUserValid("medyo","sprint"));
+    }
+
+    @Test
+    public void test2(){
+        guru_login check = new guru_login();
+        assertEquals("Login is valid", true, check.isUserValid("wrong","mistake"));
     }
 }
