909923ebe9a8f3c122a8732d6cd34d3692340921
JOEM BAUTISTA <bautisj@ph.ibm.com>
2019-03-29 16:15:20 +0800

    FAIL

--- src/main/java/guru_login.java
+++ src/main/java/guru_login.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class guru_login
  */
 
-/** THIS FILE IS SUCCESSFUL IN JUNUIT**/
+/** THIS FILE IS FAILED IN JUNUIT**/
 public class guru_login extends HttpServlet {
  
     public guru_login() {
@@ -42,13 +42,14 @@ public class guru_login extends HttpServlet {
 		}
 	}
 
-/** THIS FILE IS SUCCESSFUL IN JUNUIT**/
+/** THIS FILE IS FAILED IN JUNUIT**/
 	public boolean isUserValid(String username, String password) {
     	boolean isValid = false;
 
 		if((username.equalsIgnoreCase("MEDYO") && password.equalsIgnoreCase("SPRINT"))||
 				(username.equalsIgnoreCase("USER") && password.equalsIgnoreCase("USER"))||
-				(username.equalsIgnoreCase("VALID") && password.equalsIgnoreCase("VALID"))
+				(username.equalsIgnoreCase("USER") && password.equalsIgnoreCase("USER"))/*||
+				(username.equalsIgnoreCase("VALID") && password.equalsIgnoreCase("VALID"))*/
         )
 
 		{
